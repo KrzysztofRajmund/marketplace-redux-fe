7c8f801dee4ff6b0e30af1a3dfb0aadafc99645a
Krzysztof Nadlonek <knadlonek@gmail.com>
2020-01-26 15:40:18 +0100

    modified component navMain

--- src/components/navbar/navMain.jsx
+++ src/components/navbar/navMain.jsx
@@ -12,35 +12,59 @@ import {
 
 const NavMain = () => {
   return (
-<Navbar bg="dark" expand="lg">
-  <Navbar.Brand href="#home">LOGO</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <NavDropdown title="Shop" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="About" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-      <NavDropdown title="Log in" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+    <Navbar bg="dark" expand="lg" sticky="top">
+        {/* LOGO */}
+      <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+            {/* DROPDOWN ONE */}
+          <NavDropdown title="Shop" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Game nr 1</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Game nr 2</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Game nr 3</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Premier</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">Bestseller</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">
+              Top 4 promotions
+            </NavDropdown.Item>
+          </NavDropdown>
+          {/* DROPDOWN TWO */}
+          <NavDropdown title="About" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">shop.com</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Work with us</NavDropdown.Item>
+          </NavDropdown>
+          {/* DROPDOWN THREE */}
+          <NavDropdown title="Log in" id="basic-nav-dropdown">
+              {/* LOG IN /  SIGN UP */}
+            <NavDropdown.Item href="#action/3.1">
+              <Button>Log in </Button>
+              <Button>Register</Button>
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+                {/* CARD */}
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        {/* SHOPPING ICON + SEARCH ICON */}
+        <Nav className="mr-5">
+      <Nav.Link href="#home">Shopping Icon</Nav.Link>
+      <Nav.Link href="#link">Search Icon</Nav.Link>
     </Nav>
-  </Navbar.Collapse>
-</Navbar>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
