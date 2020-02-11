9815a3c2f40a4a6d4516a7d52275fc3a1f0f6045
Krzysztof Nadlonek <knadlonek@gmail.com>
2020-02-11 22:57:05 +0100

    created thumbnailCarousel for productDetail page

---
+++ src/components/productsPage/thumbnailCarousel.jsx
@@ -0,0 +1,101 @@
+import React, {Component, useState, useEffect} from 'react';
+import {Container,Carousel,Button,Card,CardGroup} from 'react-bootstrap'
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {getItems} from '../../actions/fetchActions';
+import { Link } from 'react-router-dom';
+
+
+
+
+const ThumbnailCarousel = ({getItems,fetchReducer }) => {
+
+    useEffect (()=>{
+        getItems();
+    },[])
+
+    const [index, setIndex] = useState(0);
+    const [direction, setDirection] = useState(null);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+      setDirection(e.direction);
+    };
+
+    const suggestedItems = fetchReducer.slice(0, 4).map(item => (
+      <Carousel.Item>
+        <CardGroup>
+          {/* <Card className="cardGroup" style={{ width: '16rem', height: '6rem'}} key={item.id}>
+        <Card.Img 
+          className="d-block w-100"
+          src={item.thumbnailUrl}
+          height="150px" 
+        />
+      </Card> */}
+          <Card className="cardGroup bg-dark text-white" 
+          style={{ width: '16rem', height: '6rem'}} key={item.id}>
+            <Card.Img 
+             className="d-block w-100"
+             src={item.thumbnailUrl}
+             height="150px"
+             />
+            <Card.ImgOverlay>
+                Product title
+            </Card.ImgOverlay>
+          </Card>
+          <Card className="cardGroup bg-dark text-white" 
+          style={{ width: '16rem', height: '6rem'}} key={item.id}>
+            <Card.Img 
+             className="d-block w-100"
+             src={item.thumbnailUrl}
+             height="150px"
+             />
+            <Card.ImgOverlay>
+                Product title
+            </Card.ImgOverlay>
+          </Card>
+          <Card className="cardGroup bg-dark text-white" 
+          style={{ width: '16rem', height: '6rem'}} key={item.id}>
+            <Card.Img 
+             className="d-block w-100"
+             src={item.thumbnailUrl}
+             height="150px"
+             />
+            <Card.ImgOverlay>
+                Product title
+            </Card.ImgOverlay>
+          </Card>
+          <Card className="cardGroup bg-dark text-white" 
+          style={{ width: '16rem', height: '6rem'}} key={item.id}>
+            <Card.Img 
+             className="d-block w-100"
+             src={item.thumbnailUrl}
+             height="150px"
+             />
+            <Card.ImgOverlay>
+                Product title
+            </Card.ImgOverlay>
+          </Card>
+        </CardGroup>
+      </Carousel.Item>
+    ));
+    return (
+      <>
+        <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
+            {suggestedItems}
+        </Carousel>
+        </>
+      );
+}
+
+
+ThumbnailCarousel.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    fetchReducer: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+fetchReducer: state.fetchReducer.items
+});
+
+export default connect (mapStateToProps,{getItems})(ThumbnailCarousel);
